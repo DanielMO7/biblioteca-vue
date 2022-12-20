c398582d3595cfa221ef09dc3924d7fa6da447fe
DanielMO7 <dani019jorje@gmail.com>
2022-12-20 14:13:21 -0500

    Organizando formulario y agrenda validac de passw

--- src/modules/register/RegisterView.vue
+++ src/modules/register/RegisterView.vue
@@ -1,152 +1,190 @@
 <template>
-    <v-container>
-    <v-card
-      class="flex"
-      flat
-      tile
-      
-    >
-    <div>
-    <v-row style="justify-content:center; margin: 0px; margin-bottom: 50px;">
+  <v-container>
+    <v-card class="flex" flat tile>
+      <v-row style="justify-content: center; margin: 0px; margin-bottom: 50px">
         <v-card max-width="374">
-            <v-img
+          <v-img
             height="550"
             width="250"
             src="../../assets/pexels-karolina-grabowska-4963815.jpg"
-            >
-            <v-card-title class="justify-center" style="background: #A52A2A; ">
-                <h5>Registrarce</h5> 
+          >
+            <v-card-title class="justify-center" style="background: #a52a2a">
+              <h5>Registrarse</h5>
             </v-card-title>
-            </v-img>
-            
+          </v-img>
         </v-card>
-
-        <v-card
-        width="400"
-        >
-            
-            <v-card-text style="margin-top: 78px">
-            <br>
-                <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-  <v-text-field
-      v-model="nombre"
-      :rules="nameRules"
-      label="Nombre"
-      required
-      outlined
-      dense
-    ></v-text-field>
-    <v-text-field
-      v-model="documento"
-      :rules="documentoRules"
-      label="Documento"
-      required
-      outlined
-      dense
-      hide-details
-      single-line
-      type="number"
-    ></v-text-field>
-    <br>
-    <v-text-field
-        width=""
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-      outlined
-      dense
-    ></v-text-field>
-
-    <v-text-field
-      v-model="password"
-      :counter="10"
-      :rules="passwordRules"
-      :type="show1 ? 'text' : 'password'"
-      label="Contraseña"
-      required
-      outlined
-      dense
-    ></v-text-field>
-    
-<br>
-
-<v-card-actions class="justify-center">
-    <v-btn
-      :disabled="!valid"
-      color="#A52A2A"
-      class="mr-4 white--text"
-      @click="validate"
-    >
-      Ingresar
-    </v-btn>
-    <v-btn
-      color="#A52A2A"
-      class="mr-4 white--text"
-      @click="validate"
-      style="color: white;"
-    >
-      Cancelar
-    </v-btn>
-
-</v-card-actions>
-  </v-form>
-            </v-card-text>
+        <v-card width="400">
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-container class="mt-3">
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    v-model="nombre"
+                    :rules="nombreRules"
+                    label="Nombre"
+                    required
+                    outlined
+                    dense
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    v-model="documento"
+                    :rules="documentoRules"
+                    label="Documento"
+                    required
+                    outlined
+                    dense
+                    type="number"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    width=""
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                    outlined
+                    dense
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    v-model="password"
+                    :append-icon="
+                      vista_icono_contrasena ? 'mdi-eye' : 'mdi-eye-off'
+                    "
+                    :rules="[rules.required, rules.min]"
+                    :type="vista_icono_contrasena ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Contraseña"
+                    hint="La contraseña debe contener 8 caracteres."
+                    counter
+                    outlined
+                    dense
+                    @click:append="
+                      vista_icono_contrasena = !vista_icono_contrasena
+                    "
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <!-- Verificacion Contraseña -->
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    v-model="password_verify"
+                    :append-icon="
+                      vista_icono_contrasena_verify ? 'mdi-eye' : 'mdi-eye-off'
+                    "
+                    :rules="[rules.required, rules.min]"
+                    :type="vista_icono_contrasena_verify ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Confirmación Contraseña"
+                    hint="La contraseña debe contener 8 caracteres."
+                    counter
+                    outlined
+                    dense
+                    @click:append="
+                      vista_icono_contrasena_verify =
+                        !vista_icono_contrasena_verify
+                    "
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row class="d-flex justify-center">
+                <v-card-actions>
+                  <v-col>
+                    <v-btn
+                      :disabled="!valid"
+                      color="#A52A2A"
+                      class="white--text"
+                      @click="validate"
+                    >
+                      Registrar
+                    </v-btn>
+                  </v-col>
+                  <v-col>
+                    <v-btn
+                      color="#A52A2A"
+                      class="white--text"
+                      @click="cancelar"
+                    >
+                      Cancelar
+                    </v-btn>
+                  </v-col>
+                </v-card-actions>
+              </v-row>
+            </v-container>
+          </v-form>
         </v-card>
-    
-</v-row>
-    </div>
+      </v-row>
     </v-card>
-    </v-container>
+  </v-container>
 </template>
 <script>
-  export default {
-    data: () => ({
-        
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Nombre es requerido',
-        v => /.+@.+\..+/.test(v) || 'Nombre es es invalido',
-      ],
-      documento: '',
-      documentoRules: [
-        v => !!v || 'E-mail es requerido',
-        v => /.+@.+\..+/.test(v) || 'E-mail es invalido',
-      ],
-      password: '',
-      passwordRules: [
-        v => !!v || 'La contraseña es requerida',
-        
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail es requerido',
-        v => /.+@.+\..+/.test(v) || 'E-mail es invalido',
-      ],
-      
-      
-    }),
+export default {
+  data: () => ({
+    vista_icono_contrasena: false,
+    vista_icono_contrasena_verify: false,
+    valid: true,
+    nombre: "",
+    nombreRules: [(v) => !!v || "Nombre es requerido"],
 
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
+    documento: "",
+    documentoRules: [(v) => !!v || "El documento es requeridos"],
+
+    password: "",
+    passwordRules: [(v) => !!v || "La contraseña es requerida"],
+
+    password_verify: "",
+
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail es requerido",
+      (v) => /.+@.+\..+/.test(v) || "E-mail es inválido",
+    ],
+    rules: {
+      required: (value) => !!value || "La contraseña es requerida.",
+      min: (v) => v.length >= 8 || "Mínimo 8 caracteres",
     },
-  }
+    rules_verify: {
+      verify: (password, password_verify) =>
+        password != password_verify || "Las contraseñas no coinciden.",
+    },
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+
+    verificar_contrasena() {
+      if (this.password == this.password_verify) {
+        return true;
+      }
+      return false;
+    },
+    cancelar() {
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 <style scoped>
-    h5 {
-        text-align: center;
-    font-size: 25px;
-    font-style: unset;
-    text-transform: uppercase;
-    text-shadow: 1px 1px 1px rgb(59 59 59);
-    background: #A52A2A;
-    color: white;
+h5 {
+  text-align: center;
+  font-size: 25px;
+  font-style: unset;
+  text-transform: uppercase;
+  text-shadow: 1px 1px 1px rgb(59 59 59);
+  background: #a52a2a;
+  color: white;
 }
 </style>
