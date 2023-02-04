a7a11e272d7595fee24cc425c51305855afbf306
DanielMO7 <dani019jorje@gmail.com>
2023-02-04 18:14:54 -0500

    Pagina de 404

---
+++ src/views/404View.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-card class="d-flex align-center justify-center mb-6" height="450">
+      <v-continer>
+        <v-row>
+          <v-col
+            style="font-size: 100px"
+            class="d-flex justify-center align-center"
+          >
+            404
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            style="font-size: 20px"
+            class="d-flex justify-center align-center"
+          >
+            Esta pagina no Funciona.
+          </v-col>
+        </v-row>
+      </v-continer>
+    </v-card>
+  </v-container>
+</template>
