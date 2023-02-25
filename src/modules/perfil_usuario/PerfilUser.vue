fa4786d63c7d3f760162c47d2c5e5f0d82bec2b1
DanielMO7 <dani019jorje@gmail.com>
2023-02-25 17:42:41 -0500

    Agregando perfil de usuario

--- src/modules/perfil_usuario/PerfilUser.vue
+++ src/modules/perfil_usuario/PerfilUser.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card width="100%" height="350">
+    <v-card v-if="!loading_perfil" width="100%" height="350">
       <v-toolbar class="d-flex justify-center" color="#a52a2a" dense dark>
         <v-card-title> Pefil de Usuario </v-card-title>
       </v-toolbar>
@@ -11,7 +11,7 @@
           lg="2"
           md="2"
         >
-          <v-avatar class="mt-3" color="primary" size="90">
+          <v-avatar class="mt-3" color="#1E1E1E" size="90">
             <!-- <img
                   src="https://cdn.vuetifyjs.com/images/john.jpg"
                   alt="John"
@@ -19,6 +19,8 @@
             <span class="white--text text-h5">{{ iniciales_nombre }}</span>
           </v-avatar>
         </v-col>
+        <v-divider vertical></v-divider>
+
         <v-col
           class="d-flex justify-center align-center"
           cols="12"
@@ -34,6 +36,29 @@
         </v-col>
       </v-row>
     </v-card>
+    <v-card v-else height="88%" class="d-flex align-center mb-5">
+      <v-container>
+        <v-card-text>
+          <v-row>
+            <!-- Circulo de progresa mientras se procesa la informacion -->
+            <v-col class="d-flex justify-center"
+              ><v-progress-circular
+                :width="7"
+                :size="77"
+                color="#a52a2a"
+                indeterminate
+              ></v-progress-circular>
+            </v-col>
+          </v-row>
+          <v-row>
+            <!-- Texto mientras se procesa la peticion. -->
+            <v-col class="d-flex justify-center">
+              <v-card-title>Cargando Perfil...</v-card-title>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-container>
+    </v-card>
   </v-container>
 </template>
 <script>
@@ -43,9 +68,11 @@ export default {
   data: () => ({
     usuario: [],
     iniciales_nombre: "",
+    loading_perfil: false,
   }),
   methods: {},
   async created() {
+    this.loading_perfil = true;
     await PerfilServices.PerfilUsuario().then((response) => {
       //console.log(response.data);
       this.usuario = response.data.data;
@@ -56,6 +83,7 @@ export default {
       }
       // charAt(0) | Selecciona el primer caracter de un string.
       this.iniciales_nombre = this.usuario.name.charAt(0);
+      this.loading_perfil = false;
     });
   },
 };
