b7aa58f73cfefd1e16d1c98e9b361f7d90f342f3
DanielMO7 <dani019jorje@gmail.com>
2023-03-08 16:22:17 -0500

    Agregado Service de Edicion de Peril y Contrasena

--- src/modules/perfil_usuario/services/PerfilServices.js
+++ src/modules/perfil_usuario/services/PerfilServices.js
@@ -5,5 +5,11 @@ var relative_path = "/usuario"
 export default {
     PerfilUsuario() {
         return apiClient.get(relative_path + '/perfil-usuario');
+    },
+    EditarPerfil(params) {
+        return apiClient.post(relative_path + '/editar-usuario/guardar', params);
+    },
+    EditarContrasena(params) {
+        return apiClient.post(relative_path + '/cambiar-contrasena', params);
     }
 }
