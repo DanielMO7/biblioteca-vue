52f35f3c82d39cd98754c5ab98419657296671bd
DanielMO7 <dani019jorje@gmail.com>
2023-03-08 16:20:47 -0500

    Agregado Edicion de Perfil y Edicion de Contrasena

--- src/modules/perfil_usuario/PerfilUser.vue
+++ src/modules/perfil_usuario/PerfilUser.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card v-if="!loading_perfil" width="100%" height="350">
+    <v-card v-if="!loading_perfil" width="100%" min-height="350">
       <v-toolbar class="d-flex justify-center" color="#a52a2a" dense dark>
         <v-card-title> Pefil de Usuario </v-card-title>
       </v-toolbar>
@@ -24,15 +24,50 @@
         <v-col
           class="d-flex justify-center align-center"
           cols="12"
-          lg="10"
-          md="10"
+          lg="5"
+          md="5"
         >
-          <v-card-text class="ml-0">
-            <p><b>Nombre:</b> {{ usuario.name }}</p>
-            <p><b>Documento:</b> {{ usuario.documento }}</p>
-            <p><b>Emil:</b> {{ usuario.email }}</p>
-            <p><b>Rol:</b> {{ usuario.rol }}</p>
-          </v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <v-card-text class="ml-0">
+                <p><b>Nombre:</b> {{ usuario.nombre }}</p>
+                <p><b>Documento:</b> {{ usuario.documento }}</p>
+                <p><b>Emil:</b> {{ usuario.email }}</p>
+                <p><b>Rol:</b> {{ usuario.rol }}</p>
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-divider vertical></v-divider>
+
+        <v-col
+          class="d-flex justify-center align-center"
+          cols="12"
+          lg="5"
+          md="5"
+        >
+          <v-row>
+            <v-col cols="12">
+              <p class="text-center"><b>Configuraciones:</b></p>
+            </v-col>
+            <v-col cols="12">
+              <v-btn @click="editar_perfil = true" outlined text color="black">
+                Editar Perfil
+                <v-icon right dark> mdi-account-edit-outline </v-icon>
+              </v-btn>
+            </v-col>
+            <v-col cols="12">
+              <v-btn
+                @click="cambiar_contrasena = true"
+                outlined
+                text
+                color="black"
+              >
+                Cambiar Contraseña
+                <v-icon right dark> mdi-lock </v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-card>
@@ -59,32 +94,57 @@
         </v-card-text>
       </v-container>
     </v-card>
+    <v-dialog persistent width="450" v-model="editar_perfil">
+      <EditarPerfilVue
+        @verificarStorage="verificarStorage"
+        @actualizarDatos="actualizarDatos()"
+        :usuario="usuario"
+        @CerrarDialog="editar_perfil = false"
+      />
+    </v-dialog>
+    <v-dialog persistent width="450" v-model="cambiar_contrasena">
+      <CambiarContrasenaVue
+        @verificarStorage="verificarStorage"
+        @actualizarDatos="actualizarDatos()"
+        :usuario="usuario"
+        @CerrarDialog="cambiar_contrasena = false"
+      />
+    </v-dialog>
   </v-container>
 </template>
 <script>
-import PerfilServices from "@/modules/perfil_usuario/services/PerfilServices";
+//import PerfilServices from "@/modules/perfil_usuario/services/PerfilServices";
+import EditarPerfilVue from "./components/EditarPerfil.vue";
+import SecureLS from "secure-ls";
+import CambiarContrasenaVue from "./components/CambiarContrasena.vue";
 
 export default {
+  components: {
+    EditarPerfilVue,
+    CambiarContrasenaVue,
+  },
   data: () => ({
     usuario: [],
     iniciales_nombre: "",
     loading_perfil: false,
+    editar_perfil: false,
+    cambiar_contrasena: false,
   }),
-  methods: {},
-  async created() {
-    this.loading_perfil = true;
-    await PerfilServices.PerfilUsuario().then((response) => {
-      //console.log(response.data);
-      this.usuario = response.data.data;
-      if (this.usuario.rol == 1) {
-        this.usuario.rol = "Administrador";
-      } else if (this.usuario.rol == 2) {
-        this.usuario.rol = "Usuario";
-      }
+  methods: {
+    verificarStorage() {
+      this.$emit("verificarStorage");
+    },
+    actualizarDatos() {
+      this.loading_perfil = true;
+      let ls = new SecureLS();
+      this.usuario = ls.get("profile_user").data;
       // charAt(0) | Selecciona el primer caracter de un string.
-      this.iniciales_nombre = this.usuario.name.charAt(0);
+      this.iniciales_nombre = this.usuario.nombre.charAt(0);
       this.loading_perfil = false;
-    });
+    },
+  },
+  created() {
+    this.actualizarDatos();
   },
 };
 </script>
